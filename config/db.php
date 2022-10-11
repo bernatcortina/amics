a5b39acce6fc1c8d95864a70492597d9cd7c51e6
Bernat Cortina <bernatcortina@users.noreply.github.com>
2022-10-11 15:24:49 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ return [
     // 'dsn' => 'mysql:host=127.2.94.130;dbname=amics',
     'dsn' => 'mysql:host=31.200.240.217;dbname=amics',
     'username' => 'amics',
-    // 'password' => 'amicsojc',
-    'password' => 'amicsojcjrt43',
+    // 'password' => '***********',
+    'password' => '*******',
     'charset' => 'utf8',
 ];
